dbb3f911796049f79e26bc69e7220dcecec21cfb
dethegeek <dethegeek@3134369b-abb3-481d-a5ac-911e5e94f972>
2014-04-16 18:27:19 +0000

    Fix an URL in the authors
    git-svn-id: https://forge.indepnet.net/svn/simcard/branches/0.84-bugfixes@59 3134369b-abb3-481d-a5ac-911e5e94f972

--- setup.php
+++ setup.php
@@ -116,7 +116,7 @@ function plugin_version_simcard() {
    global $LANG;
 
    $author = "<a href='http://www.elsendero.es'>El Sendero</a>";
-   $author.= ", <a href='www.teclib.com'>Walid Nouh</a>";
+   $author.= ", <a href='http://www.teclib.com'>Walid Nouh</a>";
    $author.= ", Anthony Piesset";
    $author.= ", <a href='mailto:dethegeek@gmail.com'>Dethegeek</a>";
    return array ('name'           => $LANG['plugin_simcard']['title'][1],
