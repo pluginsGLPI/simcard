b382b93224963ff452707de3e2aecfe800d8bad6
dethegeek <dethegeek@3134369b-abb3-481d-a5ac-911e5e94f972>
2014-11-14 10:14:17 +0000

    Add swedish localization. Thank you Jan
    git-svn-id: https://forge.indepnet.net/svn/simcard/branches/0.84-bugfixes@64 3134369b-abb3-481d-a5ac-911e5e94f972

---
+++ locales/sv_SE.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * @version $Id: sv_SE.php 44 2014-11-13 janfr $
+LICENSE
+
+This file is part of the simcard plugin.
+Den här filen ären del av modulen sim-kort till GLPI
+
+Modulen SIM-kort är fri programvara; du kan distribuera den och/eller förändran den
+under villkoren i GNU General Public License som publicerats av
+Free Software Foundation; antingen under version 2 av licensen; eller
+(enligt ditt val) någon annan senare version av licensformen.
+
+Modulen distribueras med förhoppningen att den ska vara användbar,
+men HELT UTAN NÅGRA GARANTIER; helt utan att ens indirekt ge några garantier för
+SÄLJBARHET eller ATT PASSA ETT VISST SYFTE.  Läs
+GNU General Public License för mer detaljer.
+
+Du borde ha fått en kopia av GNU General Public License
+tillsmmans med GLPI; tillsammans med modulen SIM-kort. Om inte hittar du den på <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------
+@package   simcard
+@author    the simcard plugin team
+@copyright Copyright (c) 2010-2011 Simcard plugin team
+@license   GPLv2+
+http://www.gnu.org/licenses/gpl.txt
+@link      https://forge.indepnet.net/projects/simcard
+@link      http://www.glpi-project.org/
+@since     2009
+---------------------------------------------------------------------- */
+
+$LANG['plugin_simcard']['title'][1] = "SIM-kort";
+$LANG['plugin_simcard'][1] = "Telefonnummer";
+$LANG['plugin_simcard'][2] = "PUK2";
+$LANG['plugin_simcard'][3] = "Pin 1";
+$LANG['plugin_simcard'][4] = "Puk";
+$LANG['plugin_simcard'][5] = "Pin 2";
+$LANG['plugin_simcard'][6] = "Storlek";
+$LANG['plugin_simcard'][7] = "Provider";
+$LANG['plugin_simcard'][8] = "IMSI";
+$LANG['plugin_simcard'][9] = "Volt";
+$LANG['plugin_simcard'][10] = "Nytt SIM-kort";
+$LANG['plugin_simcard'][11] = "Typ av SIM-kort";
+
+$LANG['plugin_simcard']['profile'][0] = "Rättighetshantering";
+$LANG['plugin_simcard']['profile'][1] = "SIM-kort";
